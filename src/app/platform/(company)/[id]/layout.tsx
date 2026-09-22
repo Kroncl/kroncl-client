ac33ce9003cf85de7d9acab1242e0b675ed6b5b0
mainbotan <ned.serafim@gmail.com>
2026-09-22 21:56:26 +0300

    fix(company invite button)

--- src/app/platform/(company)/[id]/layout.tsx
+++ src/app/platform/(company)/[id]/layout.tsx
@@ -65,7 +65,7 @@ export default async function CompanyLayout({
                     subTitle={company.name || ''} 
                     actions={[
                       {
-                        variant: 'accent',
+                        variant: 'contrast',
                         children: 'Пригласить',
                         icon: <Invitations />,
                         as: 'link',
